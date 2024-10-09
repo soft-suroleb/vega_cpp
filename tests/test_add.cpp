12cb81cd93741930f72e9163659418f4b94eba6a
Pavel Bozin <bozin2002@yandex.ru>
2024-10-09 17:43:42 +0300

    add tests

---
+++ tests/test_add.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../include/addition.hpp"
+
+TEST(Addition, Simple) {
+    EXPECT_EQ(add(10, 5), 15);
+    EXPECT_EQ(add(-4, 10), add(10, -4));
+    EXPECT_EQ(add(0, 0), 0);
+    EXPECT_EQ(add(-10, -20), -30);
+    EXPECT_EQ(add(-10, 20), 10);
+    EXPECT_EQ(add(10, -20), -10);
+}
